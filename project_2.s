a8ba0a2bd02764b1c0a1de6bba465c4f7044433a
dimi11433 <dimitricromarty@gmail.com>
2025-04-07 23:42:08 -0400

    Messed up the range of values again

--- project_2.s
+++ project_2.s
@@ -119,9 +119,9 @@ get_substring_value:
     sw $s3, 12($sp)
 
     li $s0, 0   #G sum
-    li $s1, 0  #H sum
-    li $s2, 0 #Char index(0-9)
-    li $s3, 0 #Valid digit counter 
+    li $s1, 0   #H sum
+    li $s2, 0   #Char index(0-9)
+    li $s3, 0   #Valid digit counter 
 
 
 
@@ -144,7 +144,7 @@ check_lower:
     #check lowercase a-z(ASCII 97-122)
     li $t1, 'a'
     blt $t0, $t1, check_upper
-    li $t1, 'p'
+    li $t1, 'z'
     bgt $t0, $t1, check_upper
     subu $t2, $t0, 'a'   # c - 'a'
     addi $t2, $t2, 10 #value = 10 + (c-'a')
@@ -153,7 +153,7 @@ check_upper:
     #Check uppercase A-Z(ASCII 65-91)
     li $t1, 'A'
     blt $t0, $t1, invalid
-    li $t1, 'P'
+    li $t1, 'Z'
     bgt $t0, $t1, invalid
     subu $t2, $t0, 'A' # c - 'A'
     addi $t2, $t2, 10 #value = 10 + (c- 'A')
